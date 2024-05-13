a3c143b5b45bb04cf5a6d38562e37364f2662d0b
LELautaroEmilioLotoski <lautilotoski@gmail.com>
2024-05-12 22:56:16 -0300

    binario-decimal

--- index.js
+++ index.js
@@ -1,3 +1,52 @@
 function laCajaDePandora(numero){
     // proximamente escribiremos codigo aqui
+
+    const resultados = [];
+
+    if(numero % 2 === 0) {
+        for(let i = 0; i <= numero; i++){
+            const resultado = i % 2;
+            resultados.push(resultado);
+        }
+    }else {
+        while (numero > 0) {
+            let residuo = numero % 16;
+            if (residuo < 10) {
+                resultados.push(residuo);
+            } else {
+                // Convertimos los residuos de 10 a 15 a letras A-F
+                switch (residuo) {
+                    case 10:
+                        resultados.push('A');
+                        break;
+                    case 11:
+                        resultados.push('B');
+                        break;
+                    case 12:
+                        resultados.push('C');
+                        break;
+                    case 13:
+                        resultados.push('D');
+                        break;
+                    case 14:
+                        resultados.push('E');
+                        break;
+                    case 15:
+                        resultados.push('F');
+                        break;
+                    default:
+                        break;
+                }
+            }
+            numero = Math.floor(numero / 16);
+        }
+    
+        // Convertimos la lista de dígitos a una cadena y la invertimos para obtener el orden correcto
+        return resultados.reverse().join('');
     }
+
+    return resultados;
+    }
+    
+    const numero = 17;
+    console.log(laCajaDePandora(numero));
